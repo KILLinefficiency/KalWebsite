ce50a4b8d32ab953da63ab22e5f1d15cec44396e
KILLinefficiency <shreyas.sable2166@gmail.com>
2024-12-31 16:32:34 +0530

    added tooltip component with styles

---
+++ src/lib/components/Tooltip.svelte
@@ -0,0 +1,45 @@
+<script>
+    let { word = "Tooltip Word", width = "300px", children } = $props();
+
+    let left = $derived.by(() => {
+        switch(width) {
+            case "300px":
+                return "-100%";
+            case "200px":
+                return "-50%";
+            case "100px":
+                return "0%";
+        }
+    });
+</script>
+
+<style>
+    .word {
+        position: relative;
+        display: inline-block;
+        color: var(--blue);
+    }
+
+    .tooltip {
+        font-size: 20px;
+        display: none;
+        z-index: 1;
+        position: absolute;
+        border-radius: 5px;
+        padding: 10px;
+        color: var(--fg);
+        background-color: var(--base);
+        border-bottom: 5px solid var(--blue);
+        bottom: 100%;
+        /*left: -100%;*/
+    }
+
+    .word:hover .tooltip {
+        display: block;
+    }
+</style>
+
+<span class = "word">
+    {word}
+    <div class = "tooltip" style:width style:left>{@render children()}</div>
+</span>
--- src/routes/docs/+page.svelte
+++ src/routes/docs/+page.svelte
@@ -1,33 +1,41 @@
 <script>
     import Content from "$lib/components/Content.svelte";
     import Card from "$lib/components/Card.svelte";
+    import Tooltip from "$lib/components/Tooltip.svelte";
 </script>
 
 <Content title="Kal" desc="Introduction" next="/docs/install" previous="x">
     Lorem ipsum odor amet, consectetuer adipiscing elit. Turpis in metus potenti erat cras parturient. Aliquam ut placerat himenaeos aliquam dignissim turpis tincidunt. Rhoncus ac dapibus vestibulum lectus luctus egestas dis? Massa ante vehicula inceptos amet eleifend volutpat platea. Senectus tempor facilisi elementum pulvinar lacinia tincidunt? Varius faucibus tempor viverra magna aliquam, habitant elementum est.
-
 Volutpat eleifend torquent taciti, velit rutrum aliquam bibendum ultricies ex. Leo nascetur augue etiam convallis orci vitae. Per mi aliquet vehicula odio enim eu. Mauris ut phasellus augue magna ridiculus lacinia praesent ornare. Montes sed et conubia gravida imperdiet scelerisque? Aliquam commodo consectetur purus scelerisque fames dapibus in tortor erat.
-
 Interdum leo fames tempor accumsan ligula rutrum fringilla? Iaculis nec dui orci vel ex duis luctus facilisis. Malesuada et nullam non ultricies ac aptent. Adipiscing nunc quisque massa viverra aliquam egestas. Tellus sed posuere etiam venenatis ultricies feugiat. Nec porttitor id sed ut dis mi vestibulum netus ultrices. Consectetur tincidunt curae congue facilisis sed. Ex fringilla sodales sem ullamcorper; id dis ipsum.
-
 Nascetur metus ut potenti per quisque sodales. Duis fermentum hendrerit eleifend natoque nulla fusce varius. Montes maximus tempor vel ut; malesuada dictum nec. Sollicitudin in vestibulum tortor augue lobortis vestibulum taciti. Metus molestie consequat viverra non dictumst vivamus eros. Torquent malesuada purus euismod enim dui risus. Integer blandit nec auctor maximus gravida ultricies himenaeos inceptos proin. Netus tristique dolor lectus tortor neque massa? Etiam mollis risus dictum, ligula semper torquent. Libero pulvinar platea ad nibh venenatis elit viverra.
-
 Fermentum quis ut dis gravida sagittis iaculis quis; cursus feugiat. Habitasse ridiculus netus class; tortor maecenas malesuada. Metus in eros interdum egestas phasellus. Donec vehicula blandit elementum quam orci aliquet. Tincidunt class faucibus metus dui nam. Penatibus habitasse semper arcu cubilia nostra ultrices purus. Vehicula ante aptent iaculis montes urna. Tempor varius dapibus nunc vitae elementum odio platea. Feugiat penatibus mattis, ridiculus habitant venenatis fermentum vehicula.
-
-    <Card type="warn">
-        Lorem ipsum odor amet, consectetuer adipiscing elit. Turpis in metus potenti erat cras parturient. Aliquam ut placerat himenaeos aliquam dignissim turpis tincidunt.
-    </Card>
-
-    <Card type="info">
-        Lorem ipsum odor amet, consectetuer adipiscing elit. Turpis in metus potenti erat cras parturient. Aliquam ut placerat himenaeos aliquam dignissim turpis tincidunt.
-    </Card>
+<Card type="warn">
+    Lorem ipsum odor amet, consectetuer adipiscing elit. Turpis in metus potenti erat cras parturient. Aliquam ut placerat himenaeos aliquam dignissim turpis tincidunt.
+</Card>
+<Card type="info">
+    Lorem ipsum odor amet, consectetuer adipiscing elit. Turpis in metus potenti erat cras parturient. Aliquam ut placerat himenaeos aliquam dignissim turpis tincidunt.
+</Card>
     Lorem ipsum odor amet, consectetuer adipiscing elit. Turpis in metus potenti erat cras parturient. Aliquam ut placerat himenaeos aliquam dignissim turpis tincidunt. Rhoncus ac dapibus vestibulum lectus luctus egestas dis? Massa ante vehicula inceptos amet eleifend volutpat platea. Senectus tempor facilisi elementum pulvinar lacinia tincidunt? Varius faucibus tempor viverra magna aliquam, habitant elementum est.
-
 Volutpat eleifend torquent taciti, velit rutrum aliquam bibendum ultricies ex. Leo nascetur augue etiam convallis orci vitae. Per mi aliquet vehicula odio enim eu. Mauris ut phasellus augue magna ridiculus lacinia praesent ornare. Montes sed et conubia gravida imperdiet scelerisque? Aliquam commodo consectetur purus scelerisque fames dapibus in tortor erat.
-
 Interdum leo fames tempor accumsan ligula rutrum fringilla? Iaculis nec dui orci vel ex duis luctus facilisis. Malesuada et nullam non ultricies ac aptent. Adipiscing nunc quisque massa viverra aliquam egestas. Tellus sed posuere etiam venenatis ultricies feugiat. Nec porttitor id sed ut dis mi vestibulum netus ultrices. Consectetur tincidunt curae congue facilisis sed. Ex fringilla sodales sem ullamcorper; id dis ipsum.
-
 Nascetur metus ut potenti per quisque sodales. Duis fermentum hendrerit eleifend natoque nulla fusce varius. Montes maximus tempor vel ut; malesuada dictum nec. Sollicitudin in vestibulum tortor augue lobortis vestibulum taciti. Metus molestie consequat viverra non dictumst vivamus eros. Torquent malesuada purus euismod enim dui risus. Integer blandit nec auctor maximus gravida ultricies himenaeos inceptos proin. Netus tristique dolor lectus tortor neque massa? Etiam mollis risus dictum, ligula semper torquent. Libero pulvinar platea ad nibh venenatis elit viverra.
-
 Fermentum quis ut dis gravida sagittis iaculis quis; cursus feugiat. Habitasse ridiculus netus class; tortor maecenas malesuada. Metus in eros interdum egestas phasellus. Donec vehicula blandit elementum quam orci aliquet. Tincidunt class faucibus metus dui nam. Penatibus habitasse semper arcu cubilia nostra ultrices purus. Vehicula ante aptent iaculis montes urna. Tempor varius dapibus nunc vitae elementum odio platea. Feugiat penatibus mattis, ridiculus habitant venenatis fermentum vehicula.
+
+<Tooltip word="Programming" width="300px">
+    Lorem ipsum odor amet, consectetuer adipiscing elit. Turpis in metus potenti erat cras parturient. Aliquam ut placerat himenaeos aliquam dignissim turpis tincidunt.
+    Lorem ipsum odor amet, consectetuer adipiscing elit. Turpis in metus potenti erat cras parturient. Aliquam ut placerat himenaeos aliquam dignissim turpis tincidunt.
+    Lorem ipsum odor amet, consectetuer adipiscing elit. Turpis in metus potenti erat cras parturient. Aliquam ut placerat himenaeos aliquam dignissim turpis tincidunt.
+</Tooltip>
+<Tooltip word="Programming" width="200px">
+    Lorem ipsum odor amet, consectetuer adipiscing elit. Turpis in metus potenti erat cras parturient. Aliquam ut placerat himenaeos aliquam dignissim turpis tincidunt.
+    Lorem ipsum odor amet, consectetuer adipiscing elit. Turpis in metus potenti erat cras parturient. Aliquam ut placerat himenaeos aliquam dignissim turpis tincidunt.
+    Lorem ipsum odor amet, consectetuer adipiscing elit. Turpis in metus potenti erat cras parturient. Aliquam ut placerat himenaeos aliquam dignissim turpis tincidunt.
+</Tooltip>
+<Tooltip word="Programming" width="100px">
+    Lorem ipsum odor amet, consectetuer adipiscing elit. Turpis in metus potenti erat cras parturient. Aliquam ut placerat himenaeos aliquam dignissim turpis tincidunt.
+    Lorem ipsum odor amet, consectetuer adipiscing elit. Turpis in metus potenti erat cras parturient. Aliquam ut placerat himenaeos aliquam dignissim turpis tincidunt.
+    Lorem ipsum odor amet, consectetuer adipiscing elit. Turpis in metus potenti erat cras parturient. Aliquam ut placerat himenaeos aliquam dignissim turpis tincidunt.
+</Tooltip>
+
 </Content>
