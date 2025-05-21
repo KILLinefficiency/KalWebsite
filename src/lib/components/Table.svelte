b81db4193d4a6c940b3809a811584188598f3d8e
KILLinefficiency <shreyas.sable2166@gmail.com>
2025-05-21 23:09:44 +0530

    added the Table component

---
+++ src/lib/components/Table.svelte
@@ -0,0 +1,51 @@
+<script>
+    let { header = ["Hello", "World","ABC"], rows } = $props();
+</script>
+
+<style>
+    table {
+        border-collapse: collapse;
+    }
+    th, td {
+        padding: 20px;
+        text-align: center;
+    }
+    th:first-child {
+        border-top-left-radius: 10px;
+    }
+    th:last-child {
+        border-top-right-radius: 10px;
+    }
+    tr:last-child td:first-child {
+        border-bottom-left-radius: 10px;
+    }
+    tr:last-child td:last-child {
+        border-bottom-right-radius: 10px;
+    }
+    tr:nth-child(odd) {
+        background-color: var(--base);
+    }
+    tr:not(:first-child):hover {
+        color: var(--green);
+        font-weight: bold;
+    }
+</style>
+
+<table>
+    <tbody>
+        <tr>
+            {#each header as head}
+                <th>{head}</th>
+            {/each}
+        </tr>
+
+        {#each rows as row}
+            <tr>
+                {#each row as item}
+                    <td>{item}</td>
+                {/each}
+            </tr>
+        {/each}
+    </tbody>
+</table>
+
--- src/routes/docs/install/+page.svelte
+++ src/routes/docs/install/+page.svelte
@@ -1,8 +1,15 @@
 <script>
     import Content from "$lib/components/Content.svelte";
     import Code from "$lib/components/Code.svelte";
+    import Table from "$lib/components/Table.svelte";
 </script>
 
 <Content title="Installing Kal" desc="From Github" next="/docs" previous="/docs/">
     <Code caption="Hello World" code={'stdout "Hello World". ;; print hello'} output={"Hello World"} />
+    <Table header={["Hello", "World", "Hi"]} rows={[
+        ["Clark", "Bruce", "Oliver"],
+        ["Ben", "Kevin", "Gwen"],
+        ["Peter", "Miles", "Miguel"],
+        ["Phineas", "Ferb", "Candace"]
+    ]} />
 </Content>
