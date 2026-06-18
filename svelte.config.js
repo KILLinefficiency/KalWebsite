dfe7c6989eea6770bbc392278ffa96ef7b8b6433
KILLinefficiency <shreyas.sable2166@gmail.com>
2026-06-19 01:53:07 +0530

    yaml file and adapter fix for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,20 @@
 import adapterVercel from "@sveltejs/adapter-vercel";
 import adapterStatic from "@sveltejs/adapter-static";
 
+const isGithub = process.env.TARGET === "github";
+
 const config = {
 	kit: {
-		adapter: process.env.DEPLOY_TARGET === "gh-pages"
-			? adapterStatic()
+		adapter: isGithub
+			? adapterStatic({ pages: "build" })
 			: adapterVercel(),
 
 		output: {
 			bundleStrategy: "single"
+		},
+
+		paths: {
+			base: isGithub ? process.env.BASE_PATH : ''
 		}
 	}
 };
